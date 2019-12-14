16a7fd405770c2ba9997d3e115842dbc9cb2eab8
Ariel Simulevski <ariel@simulevski.at>
2019-12-14 13:12:55 +0100

    Add project files

---
+++ MetricAggregator/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+// ReSharper disable InconsistentNaming
+
+namespace MetricAggregator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string KAFKA_BOOTSTRAP_SERVER = "KAFKA_BOOTSTRAP_SERVER";
+            const string KAFKA_TOPIC = "KAFKA_TOPIC";
+            const string METRIC_ENDPOINT = "METRIC_ENDPOINT";
+            const string METRIC_METHOD = "METRIC_METHOD";
+            const string METRIC_INTERVAL = "METRIC_INTERVAL";
+            const string METRIC_HEADERS = "METRIC_HEADERS";
+
+            string GetEnvironmentOrThrow(string env, string info)
+            {
+                var value = Environment.GetEnvironmentVariable(env);
+
+                if (value == null)
+                {
+                    throw new Exception($"Expected {info} in ENV {env}!");
+                }
+
+                return value;
+            }
+            
+            #region Setup
+
+            var server = GetEnvironmentOrThrow(KAFKA_BOOTSTRAP_SERVER, "a Kafka bootstrap server connection string");
+            var topic = GetEnvironmentOrThrow(KAFKA_TOPIC, "a Kafka topic");
+            var metricEndpoint = GetEnvironmentOrThrow(METRIC_ENDPOINT, "a metric endpoint URL");
+            var metricMethod = GetEnvironmentOrThrow(METRIC_METHOD, "a HTTP method").ToUpper();
+            var metricInterval = int.Parse(GetEnvironmentOrThrow(METRIC_INTERVAL, "an interval in ms"));
+            var metricHeader = GetEnvironmentOrThrow(METRIC_HEADERS, "HTTP headers in JSON format");
+            
+            var producerConfig = new ProducerConfig {BootstrapServers = server};
+            var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
+            
+            var client = new HttpClient();
+
+            #endregion
+
+            #region Data load
+
+            while (true)
+            {
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri(metricEndpoint),
+                
+                    //Manual switch since Enum.parse sometimes returns a default value when it really shouldn't
+                    Method = metricMethod switch
+                    {
+                        "GET" => HttpMethod.Get,
+                        "PUT" => HttpMethod.Put,
+                        "POST" => HttpMethod.Post,
+                        "DELETE" => HttpMethod.Delete,
+                        "PATCH" => HttpMethod.Patch,
+                        "OPTIONS" => HttpMethod.Options,
+                        "HEAD" => HttpMethod.Head,
+                        _ => throw new Exception($"Invalid HTTP method {metricMethod}")
+                    },
+                };
+            
+                JsonConvert.DeserializeObject<Dictionary<string, string>>(metricHeader).ToList().ForEach(entry =>
+                {
+                    request.Headers.Add(entry.Key, entry.Value);
+                });
+                
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                
+                producer.Produce(topic, new Message<Null, string>{Value = responseBody});
+                Thread.Sleep(metricInterval);
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ MetricCollector/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+using Npgsql;
+using NpgsqlTypes;
+
+// ReSharper disable InconsistentNaming
+
+namespace MetricCollector
+{
+    class PointerData
+    {
+        [JsonProperty("pointer")]
+        public string Pointer;
+        
+        [JsonProperty("type")]
+        public string Type;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string KAFKA_BOOTSTRAP_SERVER = "KAFKA_BOOTSTRAP_SERVER";
+            const string KAFKA_TOPIC = "KAFKA_TOPIC";
+            const string KAFKA_GROUP_ID = "KAFKA_GROUP_ID";
+            const string JSON_POINTER_EXTRACTORS = "JSON_POINTER_EXTRACTORS";
+            const string POSTGRESQL_CONNECTION_STRING = "POSTGRESQL_CONNECTION_STRING";
+            const string POSTGRESQL_INSERT_STATEMENT = "POSTGRESQL_INSERT_STATEMENT";
+            const string ADD_TIMESTAMP = "ADD_TIMESTAMP";
+                
+            string GetEnvironmentOrThrow(string env, string info)
+            {
+                var value = Environment.GetEnvironmentVariable(env);
+
+                if (value == null)
+                {
+                    throw new Exception($"Expected {info} in ENV {env}!");
+                }
+
+                return value;
+            }
+
+            #region Setup
+
+            var server = GetEnvironmentOrThrow(KAFKA_BOOTSTRAP_SERVER, "a Kafka bootstrap server connection string");
+            var topic = GetEnvironmentOrThrow(KAFKA_TOPIC, "a Kafka topic");
+            var groupId = GetEnvironmentOrThrow(KAFKA_GROUP_ID, "a Kafka group id");
+            var pointerExtractors = JsonConvert.DeserializeObject<Dictionary<string, PointerData>>(
+                GetEnvironmentOrThrow(JSON_POINTER_EXTRACTORS, "JSON pointer extractors as JSON map"));
+            var postgresqlConnectionString =
+                GetEnvironmentOrThrow(POSTGRESQL_CONNECTION_STRING, "a PostgreSQL connection string");
+            var postgresqlInsertStatement = GetEnvironmentOrThrow(POSTGRESQL_INSERT_STATEMENT, "an insert statement");
+
+            var addTimestamp = Environment.GetEnvironmentVariable(ADD_TIMESTAMP);
+            
+            //The field of the insert statement in which the timestamp should be added in
+            var shouldAddTimestamp = addTimestamp != null;
+            
+            var conn = new NpgsqlConnection(postgresqlConnectionString);
+            
+            var consumerConfig = new ConsumerConfig{BootstrapServers = server, GroupId = groupId};
+            var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+
+            consumer.Subscribe(topic);
+
+            #endregion
+
+            #region Get data
+
+            while (true)
+            {
+                try
+                {
+                    var cr = consumer.Consume();
+                    var value = cr.Value;
+
+                    conn.Open();
+
+                    using var cmd = new NpgsqlCommand(postgresqlInsertStatement, conn);
+                    
+                    foreach (var entry in pointerExtractors.ToList())
+                    {
+                        switch (entry.Value.Type)
+                        {
+                            case "int":
+                                cmd.Parameters.AddWithValue(entry.Key, NpgsqlDbType.Integer,
+                                    JsonPointer.JsonPointer.Get<int>(value, entry.Value.Pointer));
+                                break;
+                            case "float":
+                                cmd.Parameters.AddWithValue(entry.Key, NpgsqlDbType.Double,
+                                    JsonPointer.JsonPointer.Get<double>(value, entry.Value.Pointer));
+                                break;
+                            case "bool":
+                                cmd.Parameters.AddWithValue(entry.Key, NpgsqlDbType.Boolean,
+                                    JsonPointer.JsonPointer.Get<bool>(value, entry.Value.Pointer));
+                                break;
+                            case "string":
+                                cmd.Parameters.AddWithValue(entry.Key, NpgsqlDbType.Varchar,
+                                    JsonPointer.JsonPointer.Get<string>(value, entry.Value.Pointer));
+                                break;
+                        }
+                    }
+
+                    if (shouldAddTimestamp)
+                    {
+                        cmd.Parameters.AddWithValue(addTimestamp, NpgsqlDbType.Timestamp, DateTime.UtcNow);
+                    }
+
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+                catch (ConsumeException e)
+                {
+                    Console.WriteLine($"Error occured: {e.Error.Reason}");
+                }
+            }
+
+            #endregion
+
+        }
+    }
+}
